4b091bedb32c06dee4fa1d1813cb74e48648847a
Dmitry <dmixn@yandex.ru>
2023-05-06 22:03:24 +0900

    The app was inited.

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yummy/core/theme/app_colors.dart';
+import 'package:yummy/core/theme/app_theme.dart';
+import 'package:yummy/features/app_colors/presentation/bloc/colors_cubit.dart';
+import 'package:yummy/features/app_colors/presentation/screens/main_screen/main_screen.dart';
+
+import 'service_locator.dart' as di;
+
+void main() async {
+  await di.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yummy',
+      theme: const AppThemeLight().data(),
+      darkTheme: const AppThemeDark().data(),
+      color: AppColors.mainDark,
+      home: BlocProvider<ColorsCubit>(
+        create: (context) => di.sl<ColorsCubit>(),
+        child: const MainScreen(),
+      ),
+    );
+  }
+}
