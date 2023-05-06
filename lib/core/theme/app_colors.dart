9c1909e660dd7b3dfeea173ee8e3a559c216bdcd
Dmitry <dmixn@yandex.ru>
2023-05-06 21:54:35 +0900

    AppColors constants

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color mainDark = Color(0xFF0B4B5D);
+  static const Color midDark = Color(0xFF2A758D);
+  static const Color midLight = Color(0xFFA5DDF3);
+  static const Color mainLight = Color(0xFFD9FFFD);
+
+  static const Color actInfo = Color(0xFF009EB5);
+  static const Color actSuccess = Color(0xFF69C9BF);
+  static const Color actWrong = Color(0xFFBB404F);
+}
