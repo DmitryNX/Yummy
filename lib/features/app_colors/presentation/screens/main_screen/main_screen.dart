26bd5633baa225646f0bc165e28572b90596f58a
Dmitry <dmixn@yandex.ru>
2023-05-06 22:03:45 +0900

    UI: MainScreen.

---
+++ lib/features/app_colors/presentation/screens/main_screen/main_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:yummy/core/theme/app_colors.dart';
+import 'package:yummy/features/app_colors/presentation/bloc/colors_cubit.dart';
+import 'package:yummy/features/app_colors/presentation/screens/main_screen/pages/right_side_bar.dart';
+
+import 'pages/colors_palette.dart';
+import 'pages/left_side_bar.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  static const _leftBarWidth = 180.0;
+  static const _rightBarWidth = 280.0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      context.read<ColorsCubit>().getAllColorSets();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.mainLight,
+      body: Row(
+        children: const <Widget>[
+          SizedBox(
+            width: _leftBarWidth,
+            child: LeftSideBar(),
+          ),
+          Expanded(
+            // width: centerBarWidth,
+            child: ColorPalette(),
+          ),
+          SizedBox(
+            width: _rightBarWidth,
+            child: RightSideBar(),
+          ),
+        ],
+      ),
+    );
+  }
+}
