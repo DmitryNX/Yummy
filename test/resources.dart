20f4745e52ffe43b532e90fdcd6b18a71c81e271
Dmitry <dmixn@yandex.ru>
2023-05-13 21:34:13 +0900

    Test refactoring.

---
+++ test/resources.dart
@@ -0,0 +1,11 @@
+const jsonMapOfColorsEntity = {
+  'name': 'test',
+  'created': '2023-02-23T21:50:05.000',
+  'mainDark': '#FF0C2168',
+  'midDark': '#FF1437AD',
+  'midLight': '#FF425AAD',
+  'mainLight': '#FF707EAD',
+  'actInfo': '#FF1144AA',
+  'actSuccess': '#FF04849D',
+  'actWrong': '#FFD8005D',
+};
